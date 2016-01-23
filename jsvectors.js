6a943dcc3279fa1ab5d8a992ea4bfcd06cc453a9
anonymousey22 <anonymousey22@gmail.com>
2016-01-23 11:34:22 -0700

    Update jsvectors.js

--- jsvectors.js
+++ jsvectors.js
@@ -8,13 +8,16 @@ Vec.len = function(vec){
 };
 Vec.norm = function(vec){
 	var norm = Vec.len(vec);
-  return [vec[0]/norm, vec[1]/norm];
+  	return [vec[0]/norm, vec[1]/norm];
 };
 Vec.neg = function(vec){
 	return [-1*vec[0], -1*vec[0]];
 };
 Vec.add = function(vec1, vec2){
 	return [vec1[0]+vec2[0], vec1[1]+vec2[1]];
+};
+Vec.sub = function(vec1, vec2){
+  	return Vec.add(vec1, Vec.neg(vec2));
 };
 Vec.scale = function(vec, j){
 	return [vec[0]*j, vec[1]*j];
@@ -27,6 +30,6 @@ Vec.polToRec = function(theta, j){
 };
 Vec.rotate = function(vec, theta){
 	var cos = Math.cos(theta);
-  var sin = Math.sin(theta);
-  return [cos*vec[0]+-1*sin*vec[1], sin*vec[0]+cos*vec[1]];
+ 	var sin = Math.sin(theta);
+  	return [cos*vec[0]+-1*sin*vec[1], sin*vec[0]+cos*vec[1]];
 };
